fb62194cff82c54709e0af35085742fa9a77ece1
Valérian Rousset <valerian.rousset@epfl.ch>
2015-11-01 20:40:09 +0100

    to sqash

--- root_wrap.sh
+++ root_wrap.sh
@@ -1,8 +1,51 @@
 #!/bin/sh
 
-set +x
+set -eux
 
-su <<-EOF
-	./bootstrap.sh
-	cd test && ./test.sh
-EOF
+gen_uml() {
+
+	local file="${1}"
+	local script="${2}"
+
+	cat > "${file}" <<-EOF
+		#!/bin/sh
+
+		set -eux
+
+		insmod "/usr/lib/uml/modules/\`uname -r\`/kernel/fs/fuse/fuse.ko"
+		cd "${PWD}"
+
+		set +e
+		"${script}"
+		err=\$?
+		set -e
+
+		echo \${err} > "${PWD}/umltest.status"
+
+		halt -f
+	EOF
+	chmod +x "${file}"
+}
+
+run_uml() {
+	local file="${1}"
+
+	/usr/bin/linux.uml init="${file}" rootfstype=hostfs rw
+}
+
+launch_uml() {
+
+	local script="${1}"
+
+	local file="$(mktemp)"
+
+	gen_uml "${file}" "${script}"
+	run_uml "${file}"
+}
+
+case "${1}" in
+	install) launch_uml './bootstrap.sh';;
+	script)  cd test; launch_uml './test.sh';;
+esac
+
+exit $(<umltest.status)
